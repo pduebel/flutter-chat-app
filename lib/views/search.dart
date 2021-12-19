05cd2bdbd5d4dedb9d4d0f35ddf1168c138069f5
pduebel <pnduebel@gmail.com>
2021-12-19 20:55:31 +0000

    search screen update

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/services/database.dart';
 import 'package:chat_app/widgets/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -9,45 +11,62 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  TextEditingController searchController = new TextEditingController();
+  DatabaseMethods databaseMethods = DatabaseMethods();
+  TextEditingController searchController = TextEditingController();
+  QuerySnapshot? searchSnapshot;
+
+  initiateSearch() {
+    databaseMethods.getUserByUsername(searchController.text).then((val) {
+      setState(() {
+        searchSnapshot = val;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
       body: Container(
-        child: Column(children: [
-          Container(
-            color: Color(0x54FFFFFF),
-            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: searchController,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      hintText: "search username",
-                      hintStyle: TextStyle(color: Colors.white54),
-                      border: InputBorder.none,
+        child: Column(
+          children: [
+            Container(
+              color: Color(0x54FFFFFF),
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: searchController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: "search username",
+                        hintStyle: TextStyle(color: Colors.white54),
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  height: 40,
-                  width: 40,
-                  padding: EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Color(0x34FFFFFF), Color(0x0FFFFFFF)],
-                      ),
-                      borderRadius: BorderRadius.circular(40)),
-                  child: Image.asset('assets/images/search_white.png'),
-                ),
-              ],
+                  GestureDetector(
+                    onTap: () {
+                      //initiateSearch();
+                    },
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Color(0x34FFFFFF), Color(0x0FFFFFFF)],
+                          ),
+                          borderRadius: BorderRadius.circular(40)),
+                      child: Image.asset('assets/images/search_white.png'),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
